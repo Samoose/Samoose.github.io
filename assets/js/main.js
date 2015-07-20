a68c1b170903196a6d651e5ce49abf16aebc9fe3
Samoose <samposheldon@gmail.com>
2015-07-20 11:08:22 -0700

    Edits + our team
    Made non-selected images opaque when selected image is hovered on, and
    some gramatical edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,72 @@
 $(document).ready(function(){
     $(".ceo-image").hover(function(){
         $(".ceo-desc").toggle(250);
+        $(".coo").toggleClass("opacity");
+        $(".cto").toggleClass("opacity");
+        $(".cao").toggleClass("opacity");
+        $(".cio").toggleClass("opacity");
+        $(".cco").toggleClass("opacity");
+        $(".cmo").toggleClass("opacity");
     });
+    
     $(".coo-image").hover(function(){
         $(".coo-desc").toggle(250);
+        $(".ceo").toggleClass("opacity");
+        $(".cto").toggleClass("opacity");
+        $(".cao").toggleClass("opacity");
+        $(".cio").toggleClass("opacity");
+        $(".cco").toggleClass("opacity");
+        $(".cmo").toggleClass("opacity");
+        
     });
+    
     $(".cto-image").hover(function(){
         $(".cto-desc").toggle(250);
+        $(".ceo").toggleClass("opacity");
+        $(".coo").toggleClass("opacity");
+        $(".cao").toggleClass("opacity");
+        $(".cio").toggleClass("opacity");
+        $(".cco").toggleClass("opacity");
+        $(".cmo").toggleClass("opacity");
     });
+    
     $(".cao-image").hover(function(){
         $(".cao-desc").toggle(250);
+        $(".ceo").toggleClass("opacity");
+        $(".coo").toggleClass("opacity");
+        $(".cto").toggleClass("opacity");
+        $(".cio").toggleClass("opacity");
+        $(".cco").toggleClass("opacity");
+        $(".cmo").toggleClass("opacity");
     });
+    
     $(".cco-image").hover(function(){
         $(".cco-desc").toggle(250);
+        $(".ceo").toggleClass("opacity");
+        $(".coo").toggleClass("opacity");
+        $(".cto").toggleClass("opacity");
+        $(".cao").toggleClass("opacity");
+        $(".cmo").toggleClass("opacity");
+        $(".cio").toggleClass("opacity");
     });
+    
     $(".cio-image").hover(function(){
         $(".cio-desc").toggle(250);
+        $(".ceo").toggleClass("opacity");
+        $(".coo").toggleClass("opacity");
+        $(".cto").toggleClass("opacity");
+        $(".cao").toggleClass("opacity");
+        $(".cmo").toggleClass("opacity");
+        $(".cco").toggleClass("opacity");
     });
+    
     $(".cmo-image").hover(function(){
         $(".cmo-desc").toggle(250);
+        $(".ceo").toggleClass("opacity");
+        $(".coo").toggleClass("opacity");
+        $(".cto").toggleClass("opacity");
+        $(".cao").toggleClass("opacity");
+        $(".cio").toggleClass("opacity");
+        $(".cco").toggleClass("opacity");
     });
 });
